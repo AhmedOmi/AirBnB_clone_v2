63df41ab36058463b6dc796b7fb74a99c64dc6d3
AhmedOmi <ahmedomarmiledi@gmail.com>
2020-04-20 00:29:14 +0000

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,8 +10,8 @@ from datetime import datetime
 
 
 def do_pack():
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    local("sudo mkdir versions")
-    directory = "versions/web_static_{}.tgz".format(date)
-    local("tar -cvzf {} web_static/".format(directory))
+    date = datetime.now().strftime('%Y%m%d%H%M%S')
+    local('sudo mkdir versions')
+    directory = 'versions/web_static_{}.tgz'.format(date)
+    local('tar -cvzf {} web_static/'.format(directory))
     return directory
