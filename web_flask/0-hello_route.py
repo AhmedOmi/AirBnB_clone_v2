137c418fa7fa2825ad482e95496871994de614cb
AhmedOmi <ahmedomarmiledi@gmail.com>
2020-07-04 12:30:06 +0100

    fixed flask website

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 from flask import Flask
-
+"""
+import flask library
+"""
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """ function return hello hbnb """
     return 'Hello HBNB!'
 
 
